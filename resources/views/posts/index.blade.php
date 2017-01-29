fe24a77e30d27a90d94a9c058875e7bbb9dcb18f
Unknown <quentin.riviere@devinci.fr>
2017-01-29 05:47:31 +0100

    Modif vue posts/index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,31 +1,39 @@
 @extends ('layouts.app')
 
 @section('content')
-        <div class="container">
-            <div class="row">
-                <div class="page-header">
-                    <h2 class="list-group-item-heading">Articles</h2>
+    <div class="container">
+        <div class="row">
+            <div class="page-header">
+                <h2 class="list-group-item-heading">Articles</h2>
 
-                </div>
+            </div>
 
-                @if(count($posts) == 0)
-                    Aucun article n'a été publié.
-                @else
-                    @foreach($posts as $post)
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
+            @if(count($posts) == 0)
+                Aucun article n'a été publié.
+            @else
+                @foreach($posts as $post)
+                    <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
 
-                        <div class="caption">
-                            <div class="page-header info">
-                            <h3>{{ str_limit($post->title, 25) }}</h3>
-                            <br>
+                            <div class="caption">
+                                <div class="page-header info">
+                                    <h3>{{ str_limit($post->title, 20) }}</h3>
+                                    <br>
+                                </div>
+                                <p>{{ str_limit($post->content, 90) }}</p>
                             </div>
-                            <p>{{ str_limit($post->content, 90) }}</p>
                         </div>
                     </div>
-                </div>
-                    @endforeach
-                @endif
-            </div>
+                @endforeach
+            @endif
+
         </div>
+        <div class="row">
+            <div class="col-xs-6 col-xs-offset-3 col-md-5 col-md-offset-4">
+
+            {!! $posts->links() !!}
+            </div>
+
+        </div>
+    </div>
 @endsection
